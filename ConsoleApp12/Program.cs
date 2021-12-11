e1aaaa056d1cb583b82f71905c6d531fa55c5376
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-11 05:21:35 -0800

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "  Cumlede soz sayini tapmaq ucun alqoritm  ";
+            string result = String.Empty;
+            
+            string[] arr = str.Split(' ');
+            int say = 0;
+            
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != result)
+                {
+                    say++;
+                }
+            }
+            Console.WriteLine(say);
+           
+            
+        }
+    }
+}
